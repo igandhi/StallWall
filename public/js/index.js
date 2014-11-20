0658a065e0addb1ae1bd71720916e4e55d340c4e
Kaosisochukwu Uzokwe <kuzokwe@gmail.com>
2014-11-19 20:16:29 -0500

    Disables input fields when geolocation is unavailable and displays an alert to this effect

--- public/js/index.js
+++ public/js/index.js
@@ -16,7 +16,7 @@ $(function() {
 	if (navigator.geolocation) {
 		navigator.geolocation.getCurrentPosition(showPosition);
 	} else {
-		console.log('Geolocation not supported by browser');
+		disableFeaturesAndAlert();
 	}
 
 	function showPosition(position) {
@@ -80,6 +80,11 @@ $(function() {
 		return dist
 	}                                                                           
 
+	function disableFeaturesAndAlert(){
+		$inputMessage.prop('disabled', true);
+		$submit.prop('disabled', true);
+		$('.alert-box').removeClass('hide');
+	}
 
 	// keyboard events
 	$inputMessage.keydown(function (event) {
